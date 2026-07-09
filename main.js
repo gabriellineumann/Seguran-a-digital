fb1101cb0ef699c54209b61fc9a8c19a98364234
gabriellineumann <neumann.gabrielli@escola.pr.gov.br>
2026-07-09 11:49:14 -0300

    main

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-function diminuiTamanho(){
-if (tamanhoSenha > 1){
-// tamanhoSenha = tamanhoSenha-1;
-tamanhoSenha--;
-}
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
-geraSenha();
-}
-function aumentaTamanho(){
-if (tamanhoSenha < 20){
-// tamanhoSenha = tamanhoSenha+1;
-tamanhoSenha++;
-}
-numeroSenha.textContent = tamanhoSenha;
-geraSenha();
-}
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
