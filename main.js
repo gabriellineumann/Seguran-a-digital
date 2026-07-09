12addfcc912a283c1be14c6434ba13f16b6897b0
gabriellineumann <neumann.gabrielli@escola.pr.gov.br>
2026-07-09 11:51:04 -0300

    main

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 const forcaSenha = document.querySelector('.forca');
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+   
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
